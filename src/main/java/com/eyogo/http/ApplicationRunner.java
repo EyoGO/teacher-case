cb9ab36c0432d2eeb6d9660b3a41979241626feb
Yurii Hentash <gentashyura@gmail.com>
2024-12-09 14:36:59 +0200

    -35

--- src/main/java/com/eyogo/http/ApplicationRunner.java
+++ src/main/java/com/eyogo/http/ApplicationRunner.java
@@ -1,22 +1,29 @@
 package com.eyogo.http;
 
 import com.eyogo.http.config.ApplicationConfiguration;
-import com.eyogo.http.service.ImageService;
-import com.eyogo.http.service.UnitService;
-import com.eyogo.http.service.UserService;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.SpringProperties;
 
+@SpringBootApplication
 public class ApplicationRunner {
+
     public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ApplicationRunner.class, args);
+        System.out.println(context.getBeanDefinitionCount());
+
+        System.out.println(SpringProperties.getProperty("test.message"));
+        System.out.println(SpringProperties.getProperty("test.full.message"));
 
 //        try (var appContext = new ClassPathXmlApplicationContext("application.xml")) {
         // Need to close AC to call close() - trigger lifecycle callback
-        try (var appContext = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {appContext.register();
-            System.out.println(appContext.getBean("unitService"));
-            System.out.println(appContext.getBean("unitService2"));
-            System.out.println(appContext.getBean("userService"));
-            System.out.println(appContext.getBean("userService2"));
-        }
+//        try (var appContext = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
+//            System.out.println(appContext.getBean("unitService"));
+//            System.out.println(appContext.getBean("unitService2"));
+//            System.out.println(appContext.getBean("userService"));
+//            System.out.println(appContext.getBean("userService2"));
+//        }
     }
 }
---
+++ src/main/java/com/eyogo/http/config/MyJpaConfiguration.java
@@ -0,0 +1,19 @@
+/*
+package com.eyogo.http.config;
+
+import com.eyogo.http.config.condition.JpaCondition;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+
+//@Configuration
+//@Conditional(JpaCondition.class)
+public class MyJpaConfiguration {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("MyJpaConfiguration post construct");
+    }
+}
+*/
